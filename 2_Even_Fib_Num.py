ca4b184c8f9376ed0d5a1bfc4cba4729d53dbe06
Daniel <deralik.ship@gmail.com>
2022-12-22 01:27:00 -0800

    P2 Complete

--- 2_Even_Fib_Num.py
+++ 2_Even_Fib_Num.py
@@ -4,10 +4,28 @@
 Correct Answer: 
 '''
 
+BOUND = 4000000
+
 def main():
+    # Bound Check
+    if BOUND < 2:
+        return 0
+    if BOUND < 8:
+        return 2
+    if BOUND < 34:
+        return 8
+    
+    # Store current and last two Even Fib (EF-x) numbers
+    EF0, EF1, EF2 = 2, 2, 0
     total = 0
 
-    print(f"Total: {total}\n")
+    while EF0 <= BOUND:
+        total += EF0
+        EF0 = (4 * EF1) + EF2
+        EF1, EF2 = EF0, EF1
+
+    return total
 
 if __name__ == "__main__":
-    main()
+    total = main()
+    print(f"Total: {total}")
